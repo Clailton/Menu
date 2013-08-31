2fffbe4504bd53625f6631178455587dff7b0cf2
RAUL MARTINS <raul.martins@msn.com>
2013-08-31 17:12:27 -0300

    realizando o teste com comentario

--- Menu/src/main/java/br/com/empresa/BebidaController.java
+++ Menu/src/main/java/br/com/empresa/BebidaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BebidaController {
-	
+	//testando comit
 	
 	@RequestMapping(value="/bebidas")
 	public String formBebidas() {
